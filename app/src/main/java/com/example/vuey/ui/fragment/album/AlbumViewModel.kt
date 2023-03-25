99fd01be77d138d612f50339f8535a3c4ef01e4b
m4ykey <kfjdjdkh@gmail.com>
2023-03-25 19:37:58 +0100

    Album detail

--- app/src/main/java/com/example/vuey/ui/fragment/album/AlbumViewModel.kt
+++ app/src/main/java/com/example/vuey/ui/fragment/album/AlbumViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.vuey.data.local.album.detail.AlbumDetail
 import com.example.vuey.data.local.album.search.Album
 import com.example.vuey.data.repository.AlbumRepository
 import com.example.vuey.util.Resource
@@ -18,6 +19,21 @@ class AlbumViewModel @Inject constructor(
 
     private val _albumSearch = MutableLiveData<Resource<List<Album>>>()
     val albumSearch : LiveData<Resource<List<Album>>> get() = _albumSearch
+
+    private val _albumDetail = MutableLiveData<Resource<AlbumDetail>>()
+    val albumDetail : LiveData<Resource<AlbumDetail>> get() = _albumDetail
+
+    fun getInfo(artist : String, album: String) {
+        viewModelScope.launch {
+            _albumDetail.value = Resource.Loading()
+            try {
+                val response = albumRepository.getInfo(artist, album)
+                _albumDetail.value = response.data?.let { Resource.Success(it) }
+            } catch (e : Exception) {
+                _albumDetail.value = Resource.Failure("Failed to fetch album detail ${e.localizedMessage}")
+            }
+        }
+    }
 
     fun searchAlbum(album : String) {
         viewModelScope.launch {
