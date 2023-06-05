88364a253558732f071b4b9320c736ee76f98840
m4ykey <kfjdjdkh@gmail.com>
2023-06-05 14:53:42 +0200

    numbers animation of elements in statistics

--- app/src/main/java/com/example/vuey/feature_album/presentation/StatisticsAlbumFragment.kt
+++ app/src/main/java/com/example/vuey/feature_album/presentation/StatisticsAlbumFragment.kt
@@ -1,9 +1,12 @@
 package com.example.vuey.feature_album.presentation
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -16,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlin.math.max
 
 @AndroidEntryPoint
 class StatisticsAlbumFragment : Fragment() {
@@ -37,16 +41,30 @@ class StatisticsAlbumFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         hideBottomNavigation()
+
         with(binding) {
             toolBar.setNavigationOnClickListener { findNavController().navigateUp() }
 
             lifecycleScope.launch {
                 coroutineScope {
-                    val totalAlbums = viewModel.getAlbumCount().first()
-                    val totalTracks = viewModel.getTotalTracks().first()
 
-                    txtAlbumsNumber.text = totalAlbums.toString()
-                    txtSongs.text = totalTracks.toString()
+                    val startValue = 0
+
+                    val albumsEndValue = viewModel.getAlbumCount().first()
+                    val tracksEndValue  = viewModel.getTotalTracks().first()
+
+                    val valueAnimator = ValueAnimator.ofInt(startValue, max(albumsEndValue, tracksEndValue))
+
+                    valueAnimator.duration = 2000
+                    valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+
+                    valueAnimator.addUpdateListener { animator ->
+                        val animatedValue = animator.animatedValue as Int
+                        txtAlbumsNumber.text = if (animatedValue <= albumsEndValue) animatedValue.toString() else albumsEndValue.toString()
+                        txtSongs.text = if (animatedValue <= tracksEndValue) animatedValue.toString() else tracksEndValue.toString()
+                    }
+
+                    valueAnimator.start()
                 }
             }
         }
