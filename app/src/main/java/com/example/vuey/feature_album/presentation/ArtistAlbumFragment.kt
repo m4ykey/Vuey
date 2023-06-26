e6c38c1c880f254652e11833f98c20c2a6e8a297
m4ykey <kfjdjdkh@gmail.com>
2023-06-26 19:52:26 +0200

    collapsing toolbar in artist detail

--- app/src/main/java/com/example/vuey/feature_album/presentation/ArtistAlbumFragment.kt
+++ app/src/main/java/com/example/vuey/feature_album/presentation/ArtistAlbumFragment.kt
@@ -1,7 +1,6 @@
 package com.example.vuey.feature_album.presentation
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
